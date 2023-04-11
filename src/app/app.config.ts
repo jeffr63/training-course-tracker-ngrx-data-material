a9f0f927e01fc4e2b8bf93dad986c98b66079371
jeffr63 <jeffr63@gmail.com>
2023-04-11 07:49:48 -0500

    Use new ApplicationConfig bootstrap method

---
+++ src/app/app.config.ts
@@ -0,0 +1,39 @@
+import { ApplicationConfig } from '@angular/platform-browser';
+import { importProvidersFrom } from '@angular/core';
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideHttpClient } from '@angular/common/http';
+import { TitleStrategy, provideRouter } from '@angular/router';
+
+import { DefaultDataServiceConfig, EntityDataModule } from '@ngrx/data';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { StoreModule } from '@ngrx/store';
+
+import { APP_ROUTES } from './app.routes';
+import { CustomTitleStrategyService } from './services/custom-title-strategy.service';
+import { environment } from '../environments/environment';
+import { entityConfig } from './entity-metadata';
+
+const defaultDataServiceConfig: DefaultDataServiceConfig = {
+  root: 'http://localhost:3000',
+  timeout: 3000, // request timeout
+};
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    importProvidersFrom(
+      StoreModule.forRoot({}),
+      EffectsModule.forRoot([]),
+      EntityDataModule.forRoot(entityConfig),
+      StoreDevtoolsModule.instrument({
+        maxAge: 5,
+        logOnly: environment.production,
+      })
+    ),
+    { provide: DefaultDataServiceConfig, useValue: defaultDataServiceConfig },
+    { provide: TitleStrategy, useClass: CustomTitleStrategyService },
+    provideAnimations(),
+    provideHttpClient(),
+    provideRouter(APP_ROUTES),
+  ],
+};
--- src/main.ts
+++ src/main.ts
@@ -1,44 +1,12 @@
-import { enableProdMode, importProvidersFrom } from '@angular/core';
+import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import { provideHttpClient } from '@angular/common/http';
-import { provideRouter, TitleStrategy } from '@angular/router';
 
-import { StoreModule } from '@ngrx/store';
-import { EffectsModule } from '@ngrx/effects';
-import { DefaultDataServiceConfig, EntityDataModule } from '@ngrx/data';
-import { StoreDevtoolsModule } from '@ngrx/store-devtools';
-
-import { environment } from './environments/environment';
-import { APP_ROUTES } from './app/app.routes';
 import { AppComponent } from './app/app.component';
-import { CustomTitleStrategyService } from './app/services/custom-title-strategy.service';
-import { entityConfig } from './app/entity-metadata';
+import { appConfig } from './app/app.config';
+import { environment } from './environments/environment';
 
 if (environment.production) {
   enableProdMode();
 }
 
-const defaultDataServiceConfig: DefaultDataServiceConfig = {
-  root: 'http://localhost:3000',
-  timeout: 3000, // request timeout
-};
-
-bootstrapApplication(AppComponent, {
-  providers: [
-    importProvidersFrom(
-      StoreModule.forRoot({}),
-      EffectsModule.forRoot([]),
-      EntityDataModule.forRoot(entityConfig),
-      StoreDevtoolsModule.instrument({
-        maxAge: 5,
-        logOnly: environment.production,
-      })
-    ),
-    { provide: DefaultDataServiceConfig, useValue: defaultDataServiceConfig },
-    { provide: TitleStrategy, useClass: CustomTitleStrategyService },
-    provideAnimations(),
-    provideHttpClient(),
-    provideRouter(APP_ROUTES),
-  ],
-}).catch((err) => console.error(err));
+bootstrapApplication(AppComponent, appConfig).catch((err) => console.error(err));
