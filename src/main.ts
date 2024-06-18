00dfcc6b676ba64c58164af7436d56831f3ab505
jeffr63 <jeffr63@gmail.com>
2024-06-18 09:29:21 -0500

    Cleanup

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,4 @@
-import { enableProdMode } from '@angular/core';
 import { bootstrapApplication } from '@angular/platform-browser';
-
 import { AppComponent } from './app/app.component';
 import { appConfig } from './app/app.config';
 
